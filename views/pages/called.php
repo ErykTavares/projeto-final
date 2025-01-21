780f71924248dfd0eb3186f6e7695143da6d60b9
ErykTavares <erictavares35@gmail.com>
2025-01-20 22:59:34 -0300

    feat: Refactor called.php view and controller
    Refactor the called.php view and controller to improve code readability and maintainability. Clean up unnecessary code, remove redundant variable assignments, and optimize database queries. This commit also includes the implementation of the admin response feature and the creation of the admin view and adminController.

--- views/pages/called.php
+++ views/pages/called.php
@@ -1,7 +1,7 @@
 <?php
-    $token = $_GET['token'];
+$token = $_GET['token'];
 
-    echo '<h2>pagina do chamado, token: '.$token.'</h2>';
+echo '<h2>pagina do chamado, token: ' . $token . '</h2>';
 
 ?>
 
@@ -9,57 +9,57 @@
 <h2>Pergunta do suporte: <?php echo $info["ask"] ?> </h2>
 
 <?php
-    echo "<hr/>";
+echo "<hr/>";
 
-    $interactions = \MySql::startConnection()->prepare("SELECT * from call_response WHERE id_called = ? "); 
-    $interactions->execute([$token]);
-    $interactions = $interactions->fetchAll();
+$interactions = \MySql::startConnection()->prepare("SELECT * from call_response WHERE id_called = ? ");
+$interactions->execute([$token]);
+$interactions = $interactions->fetchAll();
 
-   
 
-    foreach($interactions as $key => $value){
-        if($value['user_position'] == "admin"){
-            echo "<p><b>Admin: </b>".$value["message"]."</p>";
-         
-        }else{
-            echo "<p><b>Você: </b>".$value["message"]."</p>";
-        }
-        echo "<hr/>";
+
+foreach ($interactions as $key => $value) {
+    if ($value['user_position'] == "admin") {
+        echo "<p><b>Admin: </b>" . $value["message"] . "</p>";
+
+    } else {
+        echo "<p><b>Você: </b>" . $value["message"] . "</p>";
     }
-   
+    echo "<hr/>";
+}
+
 ?>
 
 
 <?php
-    if(isset($_POST["action"])){
-        $postMessage = $_POST["message"];
-        $sql = \MySql::startConnection()->prepare("INSERT INTO call_response VALUES (NULL,?,?,?)");
-        $sql->execute([$token,$postMessage, "user"]);
+if (isset($_POST["action"])) {
+    $postMessage = $_POST["message"];
+    $sql = \MySql::startConnection()->prepare("INSERT INTO call_response VALUES (NULL,'unanswered',?,?,'user')");
+    $sql->execute([$token, $postMessage]);
 
-        echo "<script>alert('Sua resposta foi enviada! Aguarde uma resposta do administrador.')</script>";
-        echo "<script>location.href='".BASE."chamado?token=".$token."'</script>";
-        die();
-    }
+    echo "<script>alert('Sua resposta foi enviada! Aguarde uma resposta do administrador.')</script>";
+    echo "<script>location.href='" . BASE . "chamado?token=" . $token . "'</script>";
+    die();
+}
 
 
-    $sql = \MySql::startConnection()->prepare("SELECT * FROM call_response WHERE id_called = ? ORDER BY id DESC");
-    $sql->execute([$token]);
+$sql = \MySql::startConnection()->prepare("SELECT * FROM call_response WHERE id_called = ? ORDER BY id DESC");
+$sql->execute([$token]);
 
-    $message = "<h5>Aguarde uma resposta do Administrador</h5>";
+$message = "<h5>Aguarde uma resposta do Administrador</h5>";
 
-    if($sql->rowCount() == 0){
-        echo  $message;
-    }else{
-        $info = $sql->fetchAll();
-        if($info[0]['user_position'] !== "admin"){
-             echo  $message;
-        }else{
-                echo '  <form method="post">
+if ($sql->rowCount() == 0) {
+    echo $message;
+} else {
+    $info = $sql->fetchAll();
+    if ($info[0]['user_position'] !== "admin") {
+        echo $message;
+    } else {
+        echo '  <form method="post">
                             <textarea name="message" placeholder="Sua Resposta"></textarea>
                             <br />
                             <input type="submit" name="action" value="Enviar">
                         </form>';
-        }
-        
     }
+
+}
 ?>
